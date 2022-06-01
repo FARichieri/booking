cec0d5666006a0e92981e2c83edc0267e89e7d20
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-06-01 17:44:48 -0300

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/navbar/header/Header';
+import Navbar from '../../components/navbar/Navbar';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home 
