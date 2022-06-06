09d49fdf0c8630a34ab62cb2d2135c33f24b92db
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-06-06 15:59:35 -0300

    Login, Available, Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Hotel from './pages/hotel/Hotel';
-import List from './pages/list/List';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/hotels' element={<List />} />
-      <Route path='/hotel/:id' element={<Hotel />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotel/:id" element={<Hotel />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -11,7 +11,7 @@ import useFetch from "../../hooks/useFetch";
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
-  const [date, setDate] = useState(location.state.date);
+  const [dates, setDates] = useState(location.state.dates);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
   const [min, setMin] = useState(undefined);
@@ -38,18 +38,18 @@ const List = () => {
               <input type="text" placeholder={destination} />
             </div>
             <div className="lsItem">
-              <label>Check-in Date</label>
+              <label>Check-in s</label>
               <span onClick={() => setOpenDate(!openDate)}>
-                {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
-                  date[0].startDate,
+                {`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                  dates[0].startDate,
                   "MM/dd/yyyy"
                 )}`}
               </span>
               {openDate && (
                 <DateRange
-                  onChange={(item) => setDate([item.selection])}
+                  onChange={(item) => setDates([item.selection])}
                   minDate={new Date()}
-                  ranges={date}
+                  ranges={dates}
                 />
               )}
             </div>
